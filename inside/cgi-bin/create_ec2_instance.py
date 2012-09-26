0a114042f3e21c7aeebbbd9add1fe1ee3c6d997c
Hugo Boyer <hugo@osrfoundation.org>
2012-09-25 18:16:55 -0700

    Adding capability to launch Team Login instance

--- inside/cgi-bin/create_ec2_instance.py
+++ inside/cgi-bin/create_ec2_instance.py
@@ -103,12 +103,176 @@ cp %s /etc/openvpn/%s
 service openvpn start
 """
 
+TEAM_LOGIN_STARTUP_SCRIPT = """#!/bin/bash
+
+echo "In the beginning was the Computer" > /home/ubuntu/STARTUP_SCRIPT_LOG
+
+# Exit on error
+set -e
+echo "config: exit on error" >> /home/ubuntu/STARTUP_SCRIPT_LOG
+
+# Overwrite the sources.list file with different content
+cat <<DELIM > /etc/apt/sources.list
+%s
+DELIM
+echo "sources.list overriden" >> /home/ubuntu/STARTUP_SCRIPT_LOG
+
+
+apt-get update
+echo "SYSTEM UPDATED" >> /home/ubuntu/STARTUP_SCRIPT_LOG
+
+echo "Installing packages" >> /home/ubuntu/STARTUP_SCRIPT_LOG
+
+# install mercurial and fetch latest version of the Team Login website
+apt-get install -y mercurial
+echo "mercurial installed" >> /home/ubuntu/STARTUP_SCRIPT_LOG
+
+
+# apt-get install -y git
+# git clone git://github.com/bmuller/mod_auth_openid.git
+# apt-get install -y build-essential
+# libtool
+# automake
+
+
+apt-get install -y apache2
+echo "apache2 installed" >> /home/ubuntu/STARTUP_SCRIPT_LOG
+
+# apt-get install -y libapache2-mod-python
+echo "apache2-mod-python installed" >> /home/ubuntu/STARTUP_SCRIPT_LOG
+
+ 
+apt-add-repository -y ppa:rye/ppa
+apt-get update
+apt-get install -y libapache2-mod-auth-openid
+echo "libapache2-mod-auth-openid 0.6 installed from ppa:rye/ppa" >> /home/ubuntu/STARTUP_SCRIPT_LOG
+
+
+
+# restart apache2 
+# /etc/init.d/apache2 restart
+
+# to list installed modules  
+# apachectl -t -D DUMP_MODULES
+
+
+echo "STARTUP COMPLETE" >> /home/ubuntu/STARTUP_SCRIPT_LOG
+
+"""
+
 def load_startup_script(distro, username, machine_id, server_ip, client_ip):
     # TODO: Make this less fragile with a proper templating language (e.g, empy)
     sources_list = open('data/sources.list-%s'%(distro)).read()
     key = common.OPENVPN_STATIC_KEY_FNAME
     startup_script = STARTUP_SCRIPT%(sources_list, username, key, server_ip, client_ip, key, key, key, key)
     return startup_script
+
+
+def create_machine_instance(credentials_ec2, 
+                            pem_key_directory, 
+                            image_id, instance_type, 
+                            username, 
+                            distro):
+    
+    
+    ec2 = common.create_ec2_proxy(credentials_ec2)
+
+    # Create key pair to use for SSH access.  Note that 
+    # create_key_pair() registers the named key with AWS.
+    uid = str(uuid.uuid1())
+    cfg_dir=os.path.join(pem_key_directory, uid)
+    if os.path.exists(cfg_dir):
+        print('Directory/file %s already exists; bailing'%(cfg_dir))
+        raise Exception('UUID creation did not meet expectations')
+    os.makedirs(cfg_dir)
+    kp_name = 'key-%s'%(uid)
+    kp = ec2.create_key_pair(kp_name)
+    kp_fname = os.path.join(cfg_dir, kp_name + '.pem')
+    # save the ssh key
+    kp.save(cfg_dir)
+    return (uid, kp_name, kp_fname)
+        
+
+
+def create_team_login_instance():
+
+    credentials_ec2 = "boto_cfg.ini"
+    pem_key_directory = "team_login_pem" 
+    image_id = "ami-137bcf7a"
+    instance_type="t1.micro"
+    security_group = "TeamLogin"
+    username = "ubuntu" 
+    distro = "precise"
+    
+
+    print("create_team_login_instance")
+    print("    BOTO file: %s" % credentials_ec2)
+    print("    pem_key_directory: %s" % pem_key_directory)
+    print("    image_id: %s" % image_id)
+    print("    instance_type: %s" % instance_type)
+    print("    security_group: %s" % security_group)
+    print("    username: %s" % username)
+    print("    distro: %s" % distro)
+#    print("startup_script",startup_script)
+
+    sources_list = open('data/sources.list-%s'%(distro)).read()
+    startup_script = TEAM_LOGIN_STARTUP_SCRIPT % (sources_list)
+    
+#    if len(startup_script) > 0 :
+#        print(startup_script)
+#        return
+    
+    uid, kp_name, kp_fname = create_machine_instance(credentials_ec2, pem_key_directory, image_id, instance_type, username, distro)
+    try:
+        # Start it up
+        #print("Load startup script: image_id %s, security_group %s" % (image_id, security_group ) )
+        ec2 = common.create_ec2_proxy(credentials_ec2)
+        res = ec2.run_instances(    image_id=image_id, 
+                                    key_name=kp_name, 
+                                    instance_type=instance_type, 
+                                    security_groups=[security_group], user_data=startup_script)
+        
+        print('Creating instance "%s\nkey file %s"'%(res.id, kp_fname))
+        
+        # Wait for it to boot to get an IP address
+        while True:
+            done = False
+            for r in ec2.get_all_instances():
+                if r.id == res.id and r.instances[0].public_dns_name:
+                    done = True
+                    break
+            if done:
+                break
+            else:
+                time.sleep(0.1)
+
+        inst = r.instances[0]
+        hostname = inst.public_dns_name
+        aws_id = inst.id
+
+        print("Associating the instance with an Elastic IP:")
+        address = ec2.allocate_address()
+        print("\t%s" % address.public_ip)
+        res = ec2.associate_address(inst.id, address.public_ip)
+        if not res:
+            raise "Elastic IP failure"
+        
+        print ("ssh -i %s ubuntu@%s" % ( kp_fname, address.public_ip) )
+        
+
+        
+
+    except Exception as e:
+        # Clean up
+        
+        if os.path.exists(kp_fname):
+            os.unlink(kp_fname)
+            
+        cfg_dir=os.path.join(pem_key_directory, uid)
+        os.rmdir(cfg_dir)
+        # re-raise
+        raise
+    
 
 def create_ec2_instance(boto_config_file,
                         output_config_dir,
@@ -134,8 +298,9 @@ def create_ec2_instance(boto_config_file,
 
     try:
         # Start it up
+        print("Load startup script: image_id %s, security_groups %s" % (image_id, security_groups ) )
         startup_script = load_startup_script(distro, username, uid, common.OV_SERVER_IP, common.OV_CLIENT_IP)
-        res = ec2.run_instances(image_id=image_id, key_name=kp_name, instance_type=instance_type, security_groups=SECURITY_GROUPS, user_data=startup_script)
+        res = ec2.run_instances(image_id=image_id, key_name=kp_name, instance_type=instance_type, security_groups=security_groups, user_data=startup_script)
         print('Creating instance %s...'%(res.id))
 
         # Wait for it to boot to get an IP address
@@ -162,15 +327,19 @@ def create_ec2_instance(boto_config_file,
         # key file because that's what we're going to scp next.
         #TODO: put a timeout in this loop
         #TODO: use Machine.test_ssh() instead of calling ssh directly
+        
+        cmd = ['ssh', '-o', 'StrictHostKeyChecking=no', '-i', kp_fname, '%s@%s'%(USERNAME, hostname), 'ls', '/%s'%(common.OPENVPN_STATIC_KEY_FNAME)]
+        print(cmd)
         while True:
-            cmd = ['ssh', '-o', 'StrictHostKeyChecking=no', '-i', kp_fname, '%s@%s'%(USERNAME, hostname), 'ls', '/%s'%(common.OPENVPN_STATIC_KEY_FNAME)]
+            #cmd = ['ssh', '-o', 'StrictHostKeyChecking=no', '-i', kp_fname, '%s@%s'%(USERNAME, hostname), 'ls', '/%s'%(common.OPENVPN_STATIC_KEY_FNAME)]
             po = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             out,err = po.communicate()
             if po.returncode == 0:
                 break
             else:
                 time.sleep(0.1)
-
+                
+        print ("retrieve the openvpn key")
         # retrieve the openvpn key
         ov_key_fname = 'openvpn-%s.key'%(uid)
         cmd = ['scp', '-o', 'StrictHostKeyChecking=no', '-i', kp_fname, str('%s@%s:/%s'%(USERNAME, hostname, common.OPENVPN_STATIC_KEY_FNAME)), os.path.join(cfg_dir, ov_key_fname)]
@@ -210,3 +379,8 @@ def create_ec2_instance(boto_config_file,
         os.rmdir(cfg_dir)
         # re-raise
         raise
+
+if __name__ == '__main__':
+    print ("create_ec2_instance::__main__\n\n")
+    create_team_login_instance()
+    
