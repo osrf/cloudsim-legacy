3357d1afaa4bc53b5e7c725bcc0542925a340dd4
Hugo Boyer <hugo@osrfoundation.org>
2012-10-09 09:30:30 -0700

    moving constants from common.py to constants.py

---
+++ inside/cgi-bin/common/constants.py
@@ -0,0 +1,33 @@
+PACKAGE_VARNAME = 'package'
+LAUNCHFILE_VARNAME = 'launchfile'
+LAUNCHARGS_VARNAME = 'launchargs'
+EMAIL_VARNAME = 'openid.ext1.value.email'
+ADMIN_EMAIL = 'cloudsim-info@osrfoundation.org'
+USER_DATABASE = '/var/www-cloudsim-auth/users'
+SESSION_DATABASE = '/var/www-cloudsim-auth/sessions'
+BOTO_CONFIG_FILE_USEAST = '/var/www-cloudsim-auth/boto-useast'
+BOTO_CONFIG_FILE_USWEST_CA = '/var/www-cloudsim-auth/boto-uswest-ca'
+
+
+OPENID_SESSION_COOKIE_NAME = 'open_id_session_id'
+#CLOUDSIM_SESSION_COOKIE_NAME = 'cloudsim_session_id'
+HTTP_COOKIE = 'HTTP_COOKIE'
+MACHINE_ID_VARNAME = 'machine_id'
+ACTION_VARNAME = 'action'
+ATTRIB_VARNAME = 'attrib'
+FILENAME_VARNAME = 'filename'
+OPENVPN_CONFIG_FNAME = 'openvpn.config'
+OPENVPN_STATIC_KEY_FNAME = 'static.key'
+HOSTNAME_FNAME = 'hostname'
+USERNAME_FNAME = 'username'
+AWS_ID_FNAME = 'aws_id'
+BOTOFILE_FNAME = 'botofile'
+DISTRO='fuerte'
+DISPLAY=':0'
+# openvpn cloud IP
+OV_SERVER_IP = '10.8.0.1'
+# openvpn client IP
+OV_CLIENT_IP = '10.8.0.2'
+
+ROS_SH_FNAME = "ros.sh"
+MACHINES_DIR = '/var/www-cloudsim-auth/machines'
