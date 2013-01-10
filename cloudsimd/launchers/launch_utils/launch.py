e4703cc3964c3890534b3ac3f5663e2ed1440c79
Hugo Boyer <hugo@osrfoundation.org>
2013-01-09 20:31:17 -0800

    refactoring the launchers for VPC

---
+++ cloudsimd/launchers/launch_utils/launch.py
@@ -0,0 +1,205 @@
+import time
+import os
+
+import logging
+import boto
+from pprint import pprint
+import json
+import subprocess
+import commands
+
+
+class LaunchException(Exception):
+    pass
+
+
+class SshClientException(Exception):
+    pass
+
+class SshClient(object):
+    def __init__(self, constellation_directory, key_name, username, ip, ):
+        self.key_fname = os.path.join(constellation_directory, "%s.pem" % key_name) 
+        self.user = '%s@%s' % (username, ip)
+        self.ssh_connect_timeout = 5
+        
+        
+    def cmd(self, cmd, extra_ssh_args=[] ): 
+        ssh_cmd = ['ssh', '-o', 'StrictHostKeyChecking=no', '-o', 'ConnectTimeout=%d'%(self.ssh_connect_timeout), '-i', self.key_fname] + extra_ssh_args + [self.user]
+        ssh_cmd.append(cmd)
+        po = subprocess.Popen(ssh_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out,err = po.communicate()
+        if po.returncode != 0:
+            raise SshClientException(out + err)
+        else:
+            return out
+    
+    def upload_file(self, local_fname, remote_fname, extra_scp_args=[]):
+        scp_cmd = ['scp', '-o', 'StrictHostKeyChecking=no', '-o', 
+                   'ConnectTimeout=%d'%(self.ssh_connect_timeout), '-i', 
+                   self.key_fname] + extra_scp_args + [local_fname,'%s:%s'%(self.user, remote_fname)]
+        scp_cmd_string = ' '.join(scp_cmd)
+        status, output = commands.getstatusoutput(scp_cmd_string)
+        if status != 0:
+            raise SshClientException('scp failed: %s'%(output))
+        else:
+            return output
+    
+    def create_file(self, text, remote_fname):
+        cmd = "cat <<DELIM > %s\n%s\nDELIM\n" % (remote_fname, text)
+        self.cmd(cmd)
+    
+    def download_file(self, local_fname, remote_fname, extra_scp_args=[]):
+        scp_cmd = ['scp', '-o', 'StrictHostKeyChecking=no', '-o', 
+                   'ConnectTimeout=%d'%(self.ssh_connect_timeout), '-i', 
+                   self.key_fname] + extra_scp_args + ['%s:%s'%(self.config.user, remote_fname), local_fname ]
+        scp_cmd_string = ' '.join(scp_cmd)
+        print(scp_cmd_string) 
+        status, output = commands.getstatusoutput(scp_cmd_string)
+        if status != 0:
+            raise SshClientException('scp failed: %s'%(output))
+        else:
+            return output
+        
+    def find_file(self, remote_path):
+        cmd = 'ls %s' %  remote_path
+        try:
+            self.cmd(cmd)
+        except SshClientException:
+            return False
+        return True
+        
+
+def log(msg):
+    try:
+        import redis
+        redis_client = redis.Redis()
+        redis_client.publish("launchers", msg)
+        logging.info(msg)
+    except:
+        print("Warning: redis not installed.")
+    print("cloudsim log> %s" % msg)
+    
+
+def get_ec2_instance(ec2conn, id):
+    reservations = ec2conn.get_all_instances()
+    instances = [i for r in reservations for i in r.instances]
+    for i in instances:
+        if i.id == id:
+            return i
+    raise LaunchException("EC2 instance %s does not exist" % id)
+
+def wait_for_multiple_machines_to_run(ec2conn, roles_to_reservations, nb_of_tries = 150):
+    """
+    returns a dictionary of running machine ids indexed by role
+    """
+    reservations_to_roles = dict((v,k) for k,v in roles_to_reservations.iteritems())
+    
+    ready_machines = {}
+    count = nb_of_tries + len(reservations_to_roles)
+    
+    while len(reservations_to_roles) > 0:
+        done = False
+        while not done:
+            time.sleep(1)
+            count = count - 1
+            print("run count down: %s " % count)
+            if count < 0:
+                msg = "timeout while waiting for EC2 machine(s) %s" % reservations_to_roles
+                raise LaunchException(msg)
+            
+            for r in ec2conn.get_all_instances():
+                reservation = r.id
+                if r.id in reservations_to_roles and r.instances[0].state == 'running':
+                    role = reservations_to_roles[reservation]
+                    aws_id = r.instances[0].id
+                    ready_machines[role] =  aws_id
+                    reservations_to_roles.pop(reservation)
+                    print 'Done launching %s (AWS %s)'%(role, aws_id)
+                    done = True
+                    break
+                
+    return ready_machines
+
+def wait_for_ssh(self, public_ip, key_file, fname='/done', username = 'ubuntu'):
+    ssh_cmd = ['ssh', '-o', 'StrictHostKeyChecking=no',
+           '-i', '%s.pem'%(key_file), '%s@%s'%(username,
+           public_ip), 'ls %s' % fname]
+    while True:
+        po = subprocess.Popen(ssh_cmd)
+        po.communicate()
+        if po.returncode == 0:
+            break
+
+
+def _domain(user_or_domain):
+    domain = user_or_domain
+    if user_or_domain.find('@') > 0:
+        domain = user_or_domain.split('@')[1]
+    return domain
+
+def set_constellation_data(user_or_domain, constellation, value, expiration = None):
+    try:
+        import redis
+        red = redis.Redis()
+        domain = _domain(user_or_domain)
+        redis_key = domain+"/" + constellation
+        
+        str = json.dumps(value)
+        red.set(redis_key, str)
+        if expiration:
+            red.expire(redis_key, expiration)
+    except Exception, e:
+        log("can't set constellation data: %s" % e)
+        
+
+def get_constellation_data(user_or_domain, constellation):
+    try:
+        import redis
+        red = redis.Redis()
+        domain = _domain(user_or_domain)
+        redis_key = domain+"/"+constellation
+        str = red.get(redis_key)
+        data = json.loads(str)
+        return data
+    except:
+        return None    
+
+
+def wait_for_multiple_machines_to_terminate(ec2conn, roles_to_aws_ids, nb_of_tries):
+    
+    ready_machines = {}
+    
+    count = nb_of_tries + len(roles_to_aws_ids)
+    aws_ids_to_roles = dict((v,k) for k,v in roles_to_aws_ids.iteritems())
+    
+    missing_machines = {}
+    for aws_id, role in aws_ids_to_roles.iteritems():
+        terminated = ec2conn.terminate_instances(instance_ids=[aws_id] )
+        if len(terminated) ==0:
+            missing_machines[role] = aws_id
+    if len(missing_machines) > 0:    
+        msg = "machine(s) %s cannot be terminated" % missing_machines
+        raise LaunchException(msg)
+    
+    while len(aws_ids_to_roles) > 0:
+        done = False
+        while not done:
+            time.sleep(1)
+            count = count - 1
+            print("terminate count down: %s " % count)
+            if count < 0:
+                msg = "timeout while terminating EC2 machine(s) %s" % reservations_to_roles
+                raise LaunchException(msg)
+            
+            reservations =  ec2conn.get_all_instances()
+            instances = [i for r in reservations for i in r.instances]
+            for instance in instances:
+                aws_id = instance.id
+                
+                if aws_id in aws_ids_to_roles:
+                    if instance.state == 'terminated':
+                        role = aws_ids_to_roles[aws_id]
+                        aws_ids_to_roles.pop(aws_id)
+                        print 'Terminated %s (AWS %s)'%(role, aws_id)
+                        done = True
+                        break
--- cloudsimd/launchers/vpc_micro_trio.py
+++ cloudsimd/launchers/vpc_micro_trio.py
@@ -1,161 +1,89 @@
-from __future__ import with_statement
+from __future__ import print_function
 
-import multiprocessing
-from common.testing import get_boto_path, get_test_path
 import unittest
-from common.machine import get_unique_short_name, find_machine,\
-    terminate_constellation, create_ec2_proxy
-import time
 import os
-from common import constants
-
-import logging
+import time
 import boto
-from pprint import pprint
+import pprint
+
+from common.machine import get_unique_short_name
 
 
-def log(msg):
-    try:
-        import redis
-        redis_client = redis.Redis()
-        redis_client.publish("launchers", msg)
-        logging.info(msg)
-    except:
-        print("Warning: redis not installed.")
-    print("cloudsim log> %s" % msg)
+
+ 
+from common.testing import get_boto_path, get_test_path
+
+from launch_utils import wait_for_multiple_machines_to_run 
+from launch_utils import wait_for_multiple_machines_to_terminate
+from launch_utils import get_ec2_instance 
+from launch_utils import log
+from launch_utils import set_constellation_data
+from launch_utils import get_constellation_data
+from launch_utils import SshClient
+
+
+
+
+def aws_connect(credentials_ec2):    
+    boto.config = boto.pyami.config.Config(credentials_ec2)
+    ec2conn = boto.connect_ec2()
+    vpcconn =  boto.connect_vpc()    
+    return ec2conn, vpcconn
+
+def launch(username, constellation_name, tags, credentials_ec2, constellation_directory, resources ):
     
-
-def get_ec2_instance(ec2conn, id):
-    reservations = ec2conn.get_all_instances()
-    instances = [i for r in reservations for i in r.instances]
-    for i in instances:
-        if i.id == id:
-            return i
-    raise LaunchException("EC2 instance %s does not exist" % id)
-
-def wait_for_multiple_machines_to_run(ec2conn, roles_to_reservations, nb_of_tries = 100):
-    """
-    returns a dictionary of running machine ids indexed by role
-    """
-    reservations_to_roles = dict((v,k) for k,v in roles_to_reservations.iteritems())
+    ec2conn, vpcconn = aws_connect(credentials_ec2)
     
-    ready_machines = {}
-    count = nb_of_tries + len(reservations_to_roles)
-    
-    while len(reservations_to_roles) > 0:
-        done = False
-        while not done:
-            time.sleep(1)
-            count = count - 1
-            print("run count down: %s " % count)
-            if count < 0:
-                msg = "timeout while waiting for EC2 machine(s) %s" % reservations_to_roles
-                raise LaunchException(msg)
-            
-            for r in ec2conn.get_all_instances():
-                reservation = r.id
-                if r.id in reservations_to_roles and r.instances[0].state == 'running':
-                    role = reservations_to_roles[reservation]
-                    aws_id = r.instances[0].id
-                    ready_machines[role] =  aws_id
-                    reservations_to_roles.pop(reservation)
-                    print 'Done launching %s (AWS %s)'%(role, aws_id)
-                    done = True
-                    break
-                
-    return ready_machines
-
-def wait_for_multiple_machines_to_terminate(ec2conn, roles_to_aws_ids, nb_of_tries):
-    
-    ready_machines = {}
-    
-    count = nb_of_tries + len(roles_to_aws_ids)
-    aws_ids_to_roles = dict((v,k) for k,v in roles_to_aws_ids.iteritems())
-    
-    missing_machines = {}
-    for aws_id, role in aws_ids_to_roles.iteritems():
-        terminated = ec2conn.terminate_instances(instance_ids=[aws_id] )
-        if len(terminated) ==0:
-            missing_machines[role] = aws_id
-    if len(missing_machines) > 0:    
-        msg = "machine(s) %s cannot be terminated" % missing_machines
-        raise LaunchException(msg)
-    
-    while len(aws_ids_to_roles) > 0:
-        done = False
-        while not done:
-            time.sleep(1)
-            count = count - 1
-            print("terminate count down: %s " % count)
-            if count < 0:
-                msg = "timeout while terminating EC2 machine(s) %s" % reservations_to_roles
-                raise LaunchException(msg)
-            
-            reservations =  ec2conn.get_all_instances()
-            instances = [i for r in reservations for i in r.instances]
-            for instance in instances:
-                aws_id = instance.id
-                
-                if aws_id in aws_ids_to_roles:
-                    if instance.state == 'terminated':
-                        role = aws_ids_to_roles[aws_id]
-                        aws_ids_to_roles.pop(aws_id)
-                        print 'Terminated %s (AWS %s)'%(role, aws_id)
-                        done = True
-                        break
-
-
-class LaunchException(Exception):
-    pass
-
-
-
-def launch(username, constellation_name, tags, credentials_ec2, root_directory, machine_name_param = None ):
-    
-    db_root_dir =os.path.dirname( os.path.dirname(root_directory))
     
     sim_machine_name = "simulator_" + constellation_name
     robot_machine_name = "robot_" + constellation_name
     router_machine_name = "router_" + constellation_name
     
-#    from cloudsim import launch as cloudsim_launch
-    
-    log("team code three")
-    
-    boto.config = boto.pyami.config.Config(credentials_ec2)
-    ec2conn = boto.connect_ec2()
-    vpcconn =  boto.connect_vpc()
+    log("trio")
     
     
-    vpc_id = vpcconn.create_vpc('10.0.0.0/24').id
+    resources['vpc_id'] = vpcconn.create_vpc('10.0.0.0/24').id
+    vpc_id = resources['vpc_id']
     print("VPC %s" % vpc_id )
-    subnet_id = vpcconn.create_subnet(vpc_id, '10.0.0.0/24').id
     
+    resources['subnet_id'] = vpcconn.create_subnet(vpc_id, '10.0.0.0/24').id
+    subnet_id = resources['subnet_id']
     
     sg_name = 'mysg-%s'%(vpc_id) 
     sg = ec2conn.create_security_group(sg_name, 'Security group for VPC %s'%(vpc_id), vpc_id)
     sg.authorize('udp', 1194, 1194, '0.0.0.0/0')   # openvpn
     sg.authorize('tcp', 22, 22, '0.0.0.0/0')   # ssh
     sg.authorize('icmp', -1, -1, '0.0.0.0/0')  # ping
-    security_group_id = sg.id
+    resources['security_group_id'] = sg.id
     
-   
-    igw_id = vpcconn.create_internet_gateway().id
+    
+    resources['igw_id'] = vpcconn.create_internet_gateway().id
+    igw_id = resources['igw_id']
     vpcconn.attach_internet_gateway(igw_id, vpc_id)
 
-    route_table_id = vpcconn.create_route_table(vpc_id).id
-    vpcconn.create_route(route_table_id, '0.0.0.0/0', igw_id)
-    route_table_association_id = vpcconn.associate_route_table(route_table_id, subnet_id)
+    resources['route_table_id'] = vpcconn.create_route_table(vpc_id).id
+    vpcconn.create_route(resources['route_table_id'], '0.0.0.0/0', igw_id)
+    resources['route_table_association_id'] = vpcconn.associate_route_table(resources['route_table_id'], subnet_id)
     elastic_ip = ec2conn.allocate_address('vpc')
+    resources['router_ip'] = elastic_ip.public_ip
     
-    eip_public_address = elastic_ip.public_ip
-    print("elastic ip %s" % eip_public_address)
-    eip_allocation_id = elastic_ip.allocation_id
+    print("elastic ip %s" % resources['router_ip'])
     
+    resources['eip_allocation_id'] = elastic_ip.allocation_id
+
     # keys
-    key_pair_name = 'key-%s'%(vpc_id)
-    key_pair = ec2conn.create_key_pair(key_pair_name)
-    key_pair.save(os.getcwd())
+    resources['router_key_pair_name'] = 'key-router-%s'%(vpc_id)
+    key_pair = ec2conn.create_key_pair(resources['router_key_pair_name'])
+    key_pair.save(constellation_directory)
+    
+    resources['robot_key_pair_name'] = 'key-robot-%s'%(vpc_id)
+    key_pair = ec2conn.create_key_pair(resources['robot_key_pair_name'])
+    key_pair.save(constellation_directory)
+    
+    resources['sim_key_pair_name'] = 'key-sim-%s'%(vpc_id)
+    key_pair = ec2conn.create_key_pair(resources['sim_key_pair_name'])
+    key_pair.save(constellation_directory)
+    
     
     TYPE='t1.micro'
     #IMAGE='ami-0d153248'
@@ -191,7 +119,7 @@ touch /done
                                          subnet_id=subnet_id,
                                          private_ip_address=TS_IP,
                                          security_group_ids=[sg.id],
-                                         key_name=key_pair_name,
+                                         key_name=resources['router_key_pair_name'] ,
                                          user_data=ROUTER_SCRIPT)
     roles_to_reservations['router'] = res.id
     
@@ -207,7 +135,7 @@ route add %s gw %s
                                  subnet_id=subnet_id,
                                  private_ip_address=ROBOT_IP,
                                  security_group_ids=[sg.id],
-                                 key_name=key_pair_name,
+                                 key_name=resources['robot_key_pair_name'] ,
                                  user_data=ROBOT_SCRIPT)
     
     roles_to_reservations['robot'] = res.id
@@ -222,50 +150,82 @@ route add %s gw %s
                                          subnet_id=subnet_id,
                                          private_ip_address=SIM_IP,
                                          security_group_ids=[sg.id],
-                                         key_name=key_pair_name,
+                                         key_name=resources['sim_key_pair_name'] ,
                                          user_data=SIM_SCRIPT)
     roles_to_reservations['simulation'] = res.id
     
     running_machines = wait_for_multiple_machines_to_run(ec2conn, roles_to_reservations, nb_of_tries = 100)
     
-    ec2conn.create_tags([running_machines['router'] ], {'Name':router_machine_name})
-    ec2conn.create_tags([running_machines['simulation'] ], {'Name':sim_machine_name})
-    ec2conn.create_tags([running_machines['robot'] ], {'Name':robot_machine_name})
+    resources['router_aws_id'] = running_machines['router']
+    resources['robot_aws_id'] = running_machines['robot']
+    resources['simulation_aws_id'] = running_machines['simulation']
+     
     
-    router_aws_id = running_machines['router']
-    ec2conn.associate_address(router_aws_id, allocation_id = eip_allocation_id)
+    router_tags = {'Name':router_machine_name}
+    router_tags.update(tags)
+    ec2conn.create_tags([resources['router_aws_id'] ], router_tags)
     
-    router_instance =  get_ec2_instance(ec2conn, router_aws_id)
+    sim_tags = {'Name':sim_machine_name}
+    sim_tags.update(tags)
+    ec2conn.create_tags([resources['simulation_aws_id'] ], sim_tags)
+    
+    robot_tags = {'Name':robot_machine_name}
+    robot_tags.update(tags)
+    ec2conn.create_tags([resources['robot_aws_id'] ], robot_tags)
+    
+    ec2conn.associate_address(resources['router_aws_id'], allocation_id = resources['eip_allocation_id'])
+    
+    router_instance =  get_ec2_instance(ec2conn, resources['router_aws_id'])
     router_instance.modify_attribute('sourceDestCheck', False)
     
-    data = {'machines': running_machines,
-            'security_group_id':security_group_id, 
-            'eip_allocation_id':eip_allocation_id, 
-            'vpc_id': vpc_id, 
-            'igw_id':igw_id , 
-            'subnet_id':subnet_id , 
-            'route_table_association_id':route_table_association_id,
-            'route_table_id':route_table_id,
-            'key_pair_name':key_pair_name }
     
+    ssh_router = SshClient(constellation_directory, resources['router_key_pair_name'], 'ubuntu', resources['router_ip'])
     
-    print("data:")
-    pprint(data)
+    done = False
+    while not done:
+        done = ssh_router.find_file("/done")
+        time.sleep(1)
     
-    terminate_vpc_micro_trio(username, constellation_name, credentials_ec2, data, root_directory)
+    ssh_router.cmd("mkdir cloudsim")
+    
+    local = os.path.join(constellation_directory, "%s.pem" % resources['robot_key_pair_name'])
+    remote = os.path.join("cloudim", "%s.pem" % resources['robot_key_pair_name']) 
+    ssh_router.upload_file(local, remote)
+    
+    local = os.path.join(constellation_directory, "%s.pem" % resources['sim_key_pair_name'])
+    remote = os.path.join("cloudim", "%s.pem" % resources['robot_key_pair_name'])
+    ssh_router.upload_file(local, remote)
+    
+    ping_gl = """#!/bin/bash
+    
+    ssh -i %s ubuntu@%s "ls -l"
+    
+    """ % (resources['sim_key_pair_name'], SIM_IP)
+    ssh_router.create_file(ping_gl, "cloudsim/ping_gl.sh")
+    
+    # router setup
+    # ssh -i key-vpc-527d3738.pem ubuntu@107.23.183.181 "mkdir cloudsim"
+    # scp -i key-vpc-527d3738.pem ./key-vpc-527d3738.pem  ubuntu@107.23.183.181:cloudsim
+
+
+    print("resources:")
+    pprint.pprint(resources)
+    
+    expiration = None
+    set_constellation_data(username, constellation_name, resources, expiration)
+    
 
 
 
-    # sim_zip_fname = os.path.join(machine.config.cfg_dir, "%s.zip" % sim_machine.config.uid)
-    # router_machine = find_machine(username, constellation_name, router_machine_name )
-def terminate_vpc_micro_trio(username, constellation_name, credentials_ec2, resources, root_directory):
-    
-    boto.config = boto.pyami.config.Config(credentials_ec2)
-    
-    ec2conn = boto.connect_ec2()
-    vpcconn =  boto.connect_vpc()
-    
+def terminate_vpc_micro_trio(username, constellation_name, credentials_ec2, root_directory):
 
+    ec2conn, vpcconn = aws_connect(credentials_ec2)    
+ 
+    resources = get_constellation_data(username,  constellation_name)
+    print("resources:")
+    pprint.pprint(resources)
+    
+    
     try:
         route_table_association_id =  resources['route_table_association_id']
         route_table_id =  resources['route_table_id']
@@ -276,20 +236,35 @@ def terminate_vpc_micro_trio(username, constellation_name, credentials_ec2, reso
         print("error cleaning up routing table: %s" % e)
     
     try:
-        running_machines =  resources['machines']
-        wait_for_multiple_machines_to_terminate(ec2conn, running_machines, 100)
-        print 'Waiting after killing instances...'
+        running_machines =  {}
+        running_machines['router'] = resources['router_aws_id']
+        running_machines['robot'] = resources['robot_aws_id']
+        running_machines['simulation'] = resources['simulation_aws_id']
+        wait_for_multiple_machines_to_terminate(ec2conn, running_machines, 150)
+        print ('Waiting after killing instances...')
         time.sleep(20.0)
     except Exception, e:
         print ("error killing instances: %s" % e)
         
     
     try:
-        key_pair_name =  resources[ 'key_pair_name']
-        ec2conn.delete_key_pair(key_pair_name)
+        router_key_pair_name =  resources[ 'router_key_pair_name']
+        ec2conn.delete_key_pair(router_key_pair_name)
     except Exception, e:
-        print("error cleaning up key %s: %s" % (key_pair_name, e))
+        print("error cleaning up router key %s: %s" % (router_key_pair_name, e))
     
+    try:
+        robot_key_pair_name =  resources[ 'robot_key_pair_name']
+        ec2conn.delete_key_pair(robot_key_pair_name)
+    except Exception, e:
+        print("error cleaning up robot key %s: %s" % (robot_key_pair_name, e))
+        
+    try:
+        sim_key_pair_name =  resources[ 'sim_key_pair_name']
+        ec2conn.delete_key_pair(sim_key_pair_name)
+    except Exception, e:
+        print("error cleaning up simulation key %s: %s" % (sim_key_pair_name, e))
+        
     try:    
         security_group_id =  resources['security_group_id' ]
         ec2conn.delete_security_group(group_id = security_group_id)
@@ -325,33 +300,50 @@ def terminate_vpc_micro_trio(username, constellation_name, credentials_ec2, reso
     
 
 
+
+
+class DbCase(unittest.TestCase):
+    
+    def test_set_get(self):
+        
+        user_or_domain = "hugo@toto.com"
+        constellation = "constellation"
+        value = {'a':1, 'b':2}
+        expiration = 25
+        set_constellation_data(user_or_domain, constellation, value, expiration)
+        
+        data = get_constellation_data(user_or_domain, constellation)
+        self.assert_(data['a'] == value['a'], "not set")
+
 class TrioCase(unittest.TestCase):
     
     
     def test_trio(self):
         
-        self.username = "toto@toto.com"
         self.constellation_name =  get_unique_short_name("test_trio_")
-       
+        
+        self.username = "toto@toto.com"
         self.credentials_ec2  = get_boto_path()
-        self.constellation_directory = "../../test_dir"
-        self.tags = {'TestCases':'vpc_micro_trio'}
+        self.tags = {'TestCase':'vpc_micro_trio', 'constellation' : self.constellation_name, 'user': self.username}
         
-        self.root_directory = get_test_path('test_vpn_micro_trio')
-        machine_name_param = None
-        launch(self.username, self.constellation_name, self.tags, self.credentials_ec2, self.root_directory, machine_name_param )
+        self.root_directory = os.path.join(get_test_path('test_vpn_micro_trio'), self.constellation_name,)
+        os.makedirs(self.root_directory)
         
-#        ls = get_machine_tag(self.username, self.constellation_name, self.sim.config.uid, "launch_state")
-#        self.assert_(ls == "running", "bad state")
-#        
-#        ls = get_machine_tag(self.username, self.constellation_name, self.team.config.uid, "launch_state")
-#        self.assert_(ls == "running", "bad state")
-     
+        self.resources = {}
+        try:
+            launch(self.username, self.constellation_name, self.tags, self.credentials_ec2, self.root_directory, self.resources )
+        finally:
+            expiration = 3600 * 24
+            set_constellation_data(self.username, self.constellation_name, self.resources, expiration)
+
     def tearDown(self):
         unittest.TestCase.tearDown(self)
-        #self.machine.terminate()
+        #self.machine.terminate() 
         
-        terminate_constellation(self.username, self.constellation_name, self.credentials_ec2, self.root_directory)
+        # self.constellation_name = 
+        
+        terminate_vpc_micro_trio(self.username, self.constellation_name, self.credentials_ec2, self.root_directory)
+        
         
     
         
