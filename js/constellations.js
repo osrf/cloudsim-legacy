2e8cd8452f9447d967ef1226a22377e73e08229d
Hugo Boyer <hugo@osrfoundation.org>
2013-03-05 17:56:43 -0800

    adding simulation task widget javascript api

--- js/constellations.js
+++ js/constellations.js
@@ -61,11 +61,7 @@ function _get_constellation_div_str(div_name, configuration_name, constellation_
     return str;
 }
 
-<<<<<<< local
 function insert_constellation_div(div_name, configuration_name, constellation_name)
-=======
-function insert_constellation_div(div_name, configuration, constellation)
->>>>>>> other
 {
     var div = document.getElementById(div_name);
     var nodes = div.childNodes;
@@ -97,33 +93,46 @@ function insert_constellation_div(div_name, configuration, constellation)
     
     var top_div = document.createElement("div");
     top_div.id = "top";
-<<<<<<< local
-=======
-//    div.insertBefore(top_div, node);
->>>>>>> other
     
-<<<<<<< local
-    var title_str = " <h3 style='margin-bottom:0; margin-top:0; color:white'><center>";
+    var title_str = " <h3 style=' margin-top:0; margin-bottom:0;'><center>";
     title_str    +=   constellation_name + " [" + configuration_name + "]</center></h3>";
     
+    top_div.style.backgroundColor ="#44497a";
+    top_div.style.borderTopLeftRadius = "15px";
+    top_div.style.borderTopRightRadius = "15px";
+    top_div.style.color = "white";
+    top_div.style.marginTop = "0";
+    top_div.style.color = "0";
+    
     top_div.innerHTML = title_str;
-=======
-    var title_div = document.createElement("div");
-    title_div.id = "title";
->>>>>>> other
     const_div.appendChild(top_div);
     
-<<<<<<< local
     // div.insertBefore(top_div, node);
+    var msg_div = document.createElement("div");
+    msg_div.id = "error";
+    msg_div.style.color = "red";
+    const_div.appendChild(msg_div);
+
+    var terminate_button= document.createElement('input');
+    terminate_button.setAttribute('type','button');
+    terminate_button.setAttribute('value','Terminate');
+
+    terminate_button.onclick =  function()
+    {   
+        var r = confirm("terminate " + constellation_name + "?");
+        if (r==false)
+        {
+            return;
+        }
+        terminate_constellation(constellation_name);
+    };
     
+    const_div.appendChild(terminate_button);
+
     var tasks_div = document.createElement("div");
     tasks_div.id = "tasks";
     const_div.appendChild(tasks_div);
-       
-    
-    
-=======
->>>>>>> other
+
     var machines_div = document.createElement("div");
     machines_div.id = "machines";
     const_div.appendChild(machines_div);
@@ -134,6 +143,43 @@ function insert_constellation_div(div_name, configuration, constellation)
     return const_div;
 }
 
+function add_task_widget(constellation_name, task_name )
+{
+    var const_div = document.getElementById(constellation_name);
+    var tasks_div = const_div.querySelector("#tasks");
+    
+    var task_div = document.createElement("div");
+    
+    
+    var start_button= document.createElement('input');
+    start_button.setAttribute('type','button');
+    start_button.setAttribute('value','Start');
+    
+    var stop_button= document.createElement('input');
+    stop_button.setAttribute('type','button');
+    stop_button.setAttribute('value','Stop');
+    
+    var edit_button= document.createElement('input');
+    edit_button.setAttribute('type','button');
+    edit_button.setAttribute('value','Edit');
+    
+    
+    task_div.appendChild(start_button);
+    task_div.appendChild(stop_button);
+    task_div.appendChild(edit_button);
+    
+    edit_button.onclick =  function()
+    {
+        alert("yesss!");
+    };
+    
+    
+    task_div.innerHTML += "" + task_name;
+    
+    // attach to page
+    tasks_div.appendChild(task_div);
+    
+}
 
 function _constellation_terminate(div_name, constellation_name)
 {
@@ -163,5 +209,51 @@ function _set_const_style(style)
     style.margin = "1%";
     style.backgroundColor = "#a8a7a7"; // f1f1f2
 }
+
+
+$( "#task-view-form" ).dialog({
+      autoOpen: false,
+      height: 300,
+      width: 350,
+      modal: true,
+      buttons: {
+        "Create an account": function() {
+          var bValid = true;
+          allFields.removeClass( "ui-state-error" );
+ 
+          bValid = bValid && checkLength( name, "username", 3, 16 );
+          bValid = bValid && checkLength( email, "email", 6, 80 );
+          bValid = bValid && checkLength( password, "password", 5, 16 );
+ 
+          bValid = bValid && checkRegexp( name, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter." );
+          // From jquery.validate.js (by joern), contributed by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
+          bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com" );
+          bValid = bValid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
+ 
+          if ( bValid ) {
+            $( "#users tbody" ).append( "<tr>" +
+              "<td>" + name.val() + "</td>" +
+              "<td>" + email.val() + "</td>" +
+              "<td>" + password.val() + "</td>" +
+            "</tr>" );
+            $( this ).dialog( "close" );
+          }
+        },
+        Cancel: function() {
+          $( this ).dialog( "close" );
+        }
+      },
+      close: function() {
+        allFields.val( "" ).removeClass( "ui-state-error" );
+      }
+    
+/* 
+    $( "#create-user" )
+      .button()
+      .click(function() {
+        $( "#task-view-form" ).dialog( "open" );
+      });
+  */    
+});
 
 
