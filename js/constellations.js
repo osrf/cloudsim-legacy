25688a0122a92cbc8accdd0aae40b5410d6587dd
Hugo Boyer <hugo@osrfoundation.org>
2013-03-08 18:43:04 -0800

    task dlg

--- js/constellations.js
+++ js/constellations.js
@@ -1,37 +1,45 @@
 
 
-function task_dialog(task_form_div)
-{
-    this.form_div = task_form_div;
-    
-    this.create_form = create_form;
-    function create_form()
-    {
-        
-    }
-}
-
 
 function create_constellations_widget(div_name)
 {
     console.log("constellations_on_load_page " + div_name);
-    
     var div = document.getElementById(div_name);
     
-    var task_dlg =  task_dialog(div);
-    
-    task_dlg.create_form();
     
     var str = "<h2>Constellations</h2>";
     str += '<div id="task-view-form" title="Task properties">';
-    str += '<>';
+    //str += '<p>Hola</p>';
+    str += '<b>Simulation properties</b><br>';
+    str += 'ROS Package<br><input type="text" name="package"></input><br>';
+    str += 'Launch file<br><input type="text" name="launch"></input><br>';
+    str += 'Arguments<br><input type="text" name="args"></input><br>';
+    str += '<b>Network properties</b><br>';
+    str += 'Target latency (ms)<br><input type="text" name="latency"></input><br>';
     str += '</div>';
-    str += '<div id="task-add-form" title="Add task"></div>';
-    
+
+//    str += '<div id="task-add-form" title="Add task">';
+//    str += '<input type="text"></input>'
+//    str += '</div>';
+
     div.innerHTML = str;
+
+//    var task_form_div = document.createElement("div");
+//    task_form_div.id = "task-add-form";
+//    task_form_div.title = "Add task";
+//
+//    var ipack = document.createElement("input"); //input element, text
+//
+//    var txtNode = document.createTextNode("Hello");
+//    ipack.setAttribute('type',"text");
+//    ipack.setAttribute('name',"package");
+//    
+//    task_form_div.appendChild(txtNode);
+//    task_form_div.appendChild(ipack);
+//    
+//    div.appendChild(task_form_div);
     
-    //var dialog_div = document.createElement("div");
-    //dialog_div.id = "task-view-form";
+
 
     $.subscribe("/constellation", function(event, data){
         if(data.constellation_name)
@@ -50,46 +58,38 @@ function create_constellations_widget(div_name)
     // initialise the form that shows the tasks properties
     $( "#task-view-form" ).dialog({
       autoOpen: false,
-      height: 300,
+      height: 400,
       width: 350,
       modal: true,
       buttons: {
-        "Update": function() {
-          var bValid = true;
-          
-          // allFields.removeClass( "ui-state-error" );
-          bValid = true;
-          
-          /*
-          bValid = bValid && checkLength( name, "username", 3, 16 );
-          bValid = bValid && checkLength( email, "email", 6, 80 );
-          bValid = bValid && checkLength( password, "password", 5, 16 );
- 
-          bValid = bValid && checkRegexp( name, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter." );
-          // From jquery.validate.js (by joern), contributed by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
-          bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com" );
-          bValid = bValid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
-          */
-        	  
-          if ( bValid ) 
-          {
-            $( "#users tbody" ).append( "<tr>" +
-              "<td>" + name.val() + "</td>" +
-              "<td>" + email.val() + "</td>" +
-              "<td>" + password.val() + "</td>" +
-            "</tr>" );
-            $( this ).dialog( "close" );
+         "Save": function() {
+         var dlg = document.querySelector("#task-view-form");
+         var inputs = dlg.querySelectorAll("input");
+         var task_data = {
+            'ros_package' : inputs[0].value,
+            'launch' : inputs[1].value,
+            'args' : inputs[2].value,
+            'latency' : inputs[3].value
+         }
+//         var ros_package = inputs[0].value;
+//         var launch = inputs[1].value;
+//         var args = inputs[2].value;
+//         var latency = inputs[3].value;
+         
+         alert(task_data.ros_package + ", " + task_data.launch + ", " + task_data.args  + ", " + task_data.latency);
+         
+         $( this ).dialog( "close" );
           }
         },
         
-        "Delete" : function() {
-            alert("What the hell are you trying to do?");
-        }
+//        "Delete" : function() {
+//            alert("What the hell are you trying to do?");
+//        },
         
       //  Cancel: function() {
       //    $( this ).dialog( "close" );
       //  }
-      },
+      //	},
       close: function() {
        //    allFields.val( "" ).removeClass( "ui-state-error" );
     	  console.log("gone");
