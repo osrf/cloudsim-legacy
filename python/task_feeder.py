b1b82ce7ac148373c04c78ba515ec17a71bec66c
Carlos Agüero <caguero@osrfoundation.org>
2013-05-02 19:18:56 -0700

    Time conversions working

--- python/task_feeder.py
+++ python/task_feeder.py
@@ -4,69 +4,109 @@
 Create a set of fake tasks for testing
 """
 
-from datetime import datetime
 import pytz
+import argparse
+import yaml
 import pprint
 
-# Dallas (SoftLayer) is the official timezone
-OFFICIAL_TIMEZONE = 'US/Central'
 
-
-def init_team_tasks(team_timezone):
+def create_task(team, runs_file):
     '''
-    Create the list of N fake tasks
+    Generate a python list containing the set of tasks for a given competition
+    '''
+    team_tasks = []
+
+    # Read runs YAML file
+    #try:
+    with open(runs_file) as runsf:
+        runs_info = yaml.load_all(runsf)
+
+        for task in runs_info:
+
+            # Get the start and stop local datetimes
+            naive_dt_start = task['local_start']
+            naive_dt_stop = task['local_stop']
+
+            local = pytz.timezone(team['timezone'])
+            local_dt_start = local.localize(naive_dt_start, is_dst=None)
+            local_dt_stop = local.localize(naive_dt_stop, is_dst=None)
+
+            # Add the UTC start/stop time of the task using the timezone
+            utc_dt_start = local_dt_start.astimezone(pytz.utc)
+            utc_dt_stop = local_dt_stop.astimezone(pytz.utc)
+
+            task['utc_start'] = utc_dt_start
+            task['utc_stop'] = utc_dt_stop
+
+            # Add the modified task to the list
+            team_tasks.append(task)
+
+    #except Exception, excep:
+    #    print ('Error reading runs file (%s): %s'
+    #           % (runs_file, repr(excep)))
+
+    return team_tasks
+
+
+def feed(teams_file, runs_file, keys_root_dir, cs_user, cs_dest_dir):
+    '''
+    Feed a set of CloudSim instances with each set of tasks.
     '''
 
-    california = pytz.timezone(team_timezone)
-    dallas = pytz.timezone(OFFICIAL_TIMEZONE)
+    '''print 'VRC Task Feeder'
+    print 'Team file:', teams_file
+    print 'Runs file:', runs_file
+    print 'Keys root dir:', keys_root_dir
+    print 'CS dest dir:', cs_dest_dir'''
 
-    #local = datetime.now(pytz.timezone("US/Pacific"))
+    # Read teams YAML file
+    #try:
+    with open(teams_file) as teamsf:
+        teams_info = yaml.load_all(teamsf)
 
-    tasks = []
-    task_id = 1
+         # Prepare the task for each team
+        for team in teams_info:
+            cs_tasks = create_task(team, runs_file)
 
-    # Day 1
-    local_start_day1 = datetime(2013, 6, 18, 8, 0, 0, tzinfo=california)
-    local_end_day1 = datetime(2013, 6, 18, 18, 0, 0, tzinfo=california)
-    official_start_day1 = local_start_day1.astimezone(dallas)
-    official_end_day1 = local_end_day1.astimezone(dallas)
+            #print 'Team:', team['team']
+            #pprint.pprint(cs_tasks)
 
-    for i in range(5):
-        task = {'id': task_id, 'name': 'This is task ' + str(task_id),
-                'start': official_start_day1, 'end': official_end_day1}
-        tasks.append(task)
-        task_id += 1
+            # Convert the CloudSim list of tasks from a python list to JSON
+            # ToDo
 
-    # Day 2
-    local_start_day2 = datetime(2013, 6, 19, 8, 0, 0, tzinfo=california)
-    local_end_day2 = datetime(2013, 6, 19, 18, 0, 0, tzinfo=california)
-    official_start_day2 = local_start_day2.astimezone(dallas)
-    official_end_day2 = local_end_day2.astimezone(dallas)
+            # Scp the JSON to the appropriate CloudSim
+            # ToDo
 
-    for i in range(5):
-        task = {'id': task_id, 'name': 'This is task ' + str(task_id),
-                'start': official_start_day2, 'end': official_end_day2}
-        tasks.append(task)
-        task_id += 1
+            # Update Redis with the new information sent
+            # ToDo
 
-    # Day 3
-    local_start_day3 = datetime(2013, 6, 20, 8, 0, 0, tzinfo=california)
-    local_end_day3 = datetime(2013, 6, 20, 18, 0, 0, tzinfo=california)
-    official_start_day3 = local_start_day3.astimezone(dallas)
-    official_end_day3 = local_end_day3.astimezone(dallas)
-    for i in range(5):
-        task = {'id': task_id, 'name': 'This is task ' + str(task_id),
-                'start': official_start_day3, 'end': official_end_day3}
-        tasks.append(task)
-        task_id += 1
-
-    '''print 'Local start day1 (California): ', str(local_start_day1)
-    print 'Local end day1 (California): ', str(local_end_day1)
-    print 'Official start day1 (Dallas): ', str(official_start_day1)
-    print 'Official end day1 (Dallas): ', str(official_end_day1)'''
-
-    return tasks
+    #except Exception, excep:
+    #    print ('Error reading teams file (%s): %s'
+    #           % (teams_file, repr(excep)))
 
 
-my_tasks = init_team_tasks('US/Pacific')
-pprint.pprint(my_tasks)
+if __name__ == '__main__':
+    # Specify command line arguments
+    parser = argparse.ArgumentParser(
+        description=('Feed every CloudSim with the task information'))
+
+    parser.add_argument('teams_file', help='YAML file with teams info')
+    parser.add_argument('runs_file', help='YAML file with runs info')
+    parser.add_argument('keys_root_dir', help='Directory with ssh keys')
+    parser.add_argument('-u', '--user', default='ubuntu',
+                        help='CloudSim user to scp the tasks')
+    parser.add_argument('CS_dest_dir',
+                        help=('CloudSim destination directory where the tasks'
+                              'will be remote copied'))
+
+    # Parse command line arguments
+    args = parser.parse_args()
+    arg_teams_file = args.teams_file
+    arg_runs_file = args.runs_file
+    arg_keys_root_dir = args.keys_root_dir
+    arg_cs_user = args.user
+    arg_cs_dest_dir = args.CS_dest_dir
+
+    # Feed the tasks!
+    feed(arg_teams_file, arg_runs_file, arg_keys_root_dir,
+         arg_cs_user, arg_cs_dest_dir)
