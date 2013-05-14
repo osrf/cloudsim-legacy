18f1b4e39d380c94665ed48d22d9862581ab2b2a
Brant Revill <michael.revill@navy.mil>
2013-05-14 15:28:58 -0400

    Themed / updated redirect to index.html

--- inside/cgi-bin/logout.py
+++ inside/cgi-bin/logout.py
@@ -12,11 +12,19 @@ out_cookies = Cookie.SmartCookie()
 out_cookies[common.OPENID_SESSION_COOKIE_NAME] = ''
 out_cookies[common.OPENID_SESSION_COOKIE_NAME]['path'] = '/cloudsim/inside/cgi-bin/'
 print(out_cookies)
-print("""Content-type: text/html
-
-<h1>Goodbye</h1>
-<a href="/cloudsim/login.html">login</a>
-<p>
-<a href="http://www.google.com/accounts/logout">Google Logout</a>
-
-""")
+print("""Content-type: text/html""")
+page = """
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="refresh" content="0;url=/cloudsim/inside/cgi-bin/home" />
+    <title>CloudSim</title>
+</head>
+<body style="text-align:center;padding-top:75px;background-color: #f7f7f7;">
+   <img src="/cloudsim/js/images/CloudSim_Logo.png" width="200px"/><br/>
+   <img src="/cloudsim/js/images/loading.gif" width="200px"/>
+</body>
+<html>
+"""
+print(page)
