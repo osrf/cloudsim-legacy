45c0527d60f6b93399e3daeef7a9634c5132b305
Steve Peters <scpeters@osrfoundation.org>
2013-05-29 11:08:14 -0700

    Put quotes around $DIR variables in bash scripts

--- cloudsimd/launchers/launch_utils/softlayer/bash/router_init.bash
+++ cloudsimd/launchers/launch_utils/softlayer/bash/router_init.bash
@@ -25,7 +25,7 @@ sudo apt-get install -y expect
 
 # --------------------------------------------
 
-cat <<DELIM > $DIR/change_ip.bash
+cat <<DELIM > "$DIR"/change_ip.bash
 
 #
 # Assumes private IP starts with 10.41. rather than look for 
@@ -52,233 +52,233 @@ echo "nameserver 8.8.4.4" >> /etc/resolv.conf
 echo done
 DELIM
 
-chmod +x $DIR/change_ip.bash
+chmod +x "$DIR"/change_ip.bash
 
 # change_ip.bash is easily run from the home directory on all machines
-cp $DIR/change_ip.bash $DIR/..
+cp "$DIR"/change_ip.bash "$DIR"/..
 
  # --------------------------------------------
 
-cat <<DELIM > $DIR/set_fc1_ip.bash
+cat <<DELIM > "$DIR"/set_fc1_ip.bash
 #!/bin/bash
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-fc1.pem $DIR/change_ip.bash ubuntu@$FC1_INITIAL_IP:change_ip.bash
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-fc1.pem ubuntu@$FC1_INITIAL_IP "nohup sudo ./change_ip.bash $FC1_IP > ssh_change_ip.out 2> ssh_change_ip.err < /dev/null &"
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-fc1.pem "$DIR"/change_ip.bash ubuntu@$FC1_INITIAL_IP:change_ip.bash
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-fc1.pem ubuntu@$FC1_INITIAL_IP "nohup sudo ./change_ip.bash $FC1_IP > ssh_change_ip.out 2> ssh_change_ip.err < /dev/null &"
 
 DELIM
-chmod +x $DIR/set_fc1_ip.bash
+chmod +x "$DIR"/set_fc1_ip.bash
 
 # --------------------------------------------
 
-cat <<DELIM > $DIR/set_fc2_ip.bash
+cat <<DELIM > "$DIR"/set_fc2_ip.bash
 #!/bin/bash
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-fc2.pem $DIR/change_ip.bash ubuntu@$FC2_INITIAL_IP:change_ip.bash
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-fc2.pem ubuntu@$FC2_INITIAL_IP "nohup sudo ./change_ip.bash $FC2_IP > ssh_change_ip.out 2> ssh_change_ip.err < /dev/null &"
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-fc2.pem "$DIR"/change_ip.bash ubuntu@$FC2_INITIAL_IP:change_ip.bash
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-fc2.pem ubuntu@$FC2_INITIAL_IP "nohup sudo ./change_ip.bash $FC2_IP > ssh_change_ip.out 2> ssh_change_ip.err < /dev/null &"
 
 DELIM
-chmod +x $DIR/set_fc2_ip.bash
+chmod +x "$DIR"/set_fc2_ip.bash
 
 # --------------------------------------------
 
-cat <<DELIM > $DIR/set_sim_ip.bash
+cat <<DELIM > "$DIR"/set_sim_ip.bash
 #!/bin/bash
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-sim.pem $DIR/change_ip.bash ubuntu@$SIM_INITIAL_IP:change_ip.bash
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-sim.pem ubuntu@$SIM_INITIAL_IP "nohup sudo ./change_ip.bash $SIM_IP > ssh_change_ip.out 2> ssh_change_ip.err < /dev/null &"
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-sim.pem "$DIR"/change_ip.bash ubuntu@$SIM_INITIAL_IP:change_ip.bash
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-sim.pem ubuntu@$SIM_INITIAL_IP "nohup sudo ./change_ip.bash $SIM_IP > ssh_change_ip.out 2> ssh_change_ip.err < /dev/null &"
 
 DELIM
-chmod +x $DIR/set_sim_ip.bash
-
-
-# --------------------------------------------
-
-cat <<DELIM > $DIR/dpkg_log_fc1.bash
-#!/bin/bash
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-fc1.pem ubuntu@$FC1_IP "tail -1 /var/log/dpkg.log"
-DELIM
-chmod +x $DIR/dpkg_log_fc1.bash
+chmod +x "$DIR"/set_sim_ip.bash
 
 
 # --------------------------------------------
 
-
-cat <<DELIM > $DIR/find_file_fc1.bash
+cat <<DELIM > "$DIR"/dpkg_log_fc1.bash
 #!/bin/bash
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-fc1.pem ubuntu@$FC1_IP "ls \$1"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-fc1.pem ubuntu@$FC1_IP "tail -1 /var/log/dpkg.log"
 DELIM
-chmod +x $DIR/find_file_fc1.bash
-
-# --------------------------------------------
-
-
-cat <<DELIM > $DIR/ssh_fc1.bash
-#!/bin/bash
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-fc1.pem ubuntu@$FC1_IP
-DELIM
-chmod +x $DIR/ssh_fc1.bash
-
-# --------------------------------------------
-
-cat <<DELIM > $DIR/fc1_init.bash
-#!/bin/bash
-set -ex
-exec > $DIR/fc1_init.log 2>&1
-
-chmod +x $DIR/fc1_startup_script.bash
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-fc1.pem $DIR/fc1_startup_script.bash ubuntu@$FC1_IP:fc1_startup_script.bash
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-fc1.pem ubuntu@$FC1_IP "nohup sudo ./fc1_startup_script.bash > ssh_startup.out 2> ssh_startup.err < /dev/null &"
-
-DELIM
-chmod +x $DIR/fc1_init.bash
-
-
-# --------------------------------------------
-
-cat <<DELIM > $DIR/reboot_fc1.bash
-#!/bin/bash
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-fc1.pem ubuntu@$FC1_IP "sudo reboot"
-DELIM
-chmod +x $DIR/reboot_fc1.bash
+chmod +x "$DIR"/dpkg_log_fc1.bash
 
 
 # --------------------------------------------
 
 
-cat <<DELIM > $DIR/dpkg_log_fc2.bash
+cat <<DELIM > "$DIR"/find_file_fc1.bash
 #!/bin/bash
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-fc2.pem ubuntu@$FC2_IP "tail -1 /var/log/dpkg.log"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-fc1.pem ubuntu@$FC1_IP "ls \$1"
 DELIM
-chmod +x $DIR/dpkg_log_fc2.bash
-
+chmod +x "$DIR"/find_file_fc1.bash
 
 # --------------------------------------------
 
 
-cat <<DELIM > $DIR/find_file_fc2.bash
+cat <<DELIM > "$DIR"/ssh_fc1.bash
 #!/bin/bash
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-fc2.pem ubuntu@$FC2_IP "ls \$1"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-fc1.pem ubuntu@$FC1_IP
 DELIM
-chmod +x $DIR/find_file_fc2.bash
+chmod +x "$DIR"/ssh_fc1.bash
 
 # --------------------------------------------
 
-
-cat <<DELIM > $DIR/ssh_fc2.bash
-#!/bin/bash
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-fc2.pem ubuntu@$FC2_IP
-DELIM
-chmod +x $DIR/ssh_fc2.bash
-
-
-# --------------------------------------------
-
-
-cat <<DELIM > $DIR/reboot_fc2.bash
-#!/bin/bash
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-fc2.pem ubuntu@$FC2_IP "sudo reboot"
-DELIM
-chmod +x $DIR/reboot_fc2.bash
-
-
-# --------------------------------------------
-
-
-cat <<DELIM > $DIR/fc2_init.bash
+cat <<DELIM > "$DIR"/fc1_init.bash
 #!/bin/bash
 set -ex
-exec > $DIR/fc2_init.log 2>&1
+exec > "$DIR"/fc1_init.log 2>&1
 
-chmod +x $DIR/fc2_startup_script.bash
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-fc2.pem $DIR/fc2_startup_script.bash ubuntu@$FC2_IP:fc2_startup_script.bash
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-fc2.pem ubuntu@$FC2_IP "nohup sudo ./fc2_startup_script.bash > ssh_startup.out 2> ssh_startup.err < /dev/null &"
+chmod +x "$DIR"/fc1_startup_script.bash
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-fc1.pem "$DIR"/fc1_startup_script.bash ubuntu@$FC1_IP:fc1_startup_script.bash
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-fc1.pem ubuntu@$FC1_IP "nohup sudo ./fc1_startup_script.bash > ssh_startup.out 2> ssh_startup.err < /dev/null &"
 
 DELIM
-chmod +x $DIR/fc2_init.bash
+chmod +x "$DIR"/fc1_init.bash
 
 
 # --------------------------------------------
 
-
-
-cat <<DELIM > $DIR/dpkg_log_sim.bash
+cat <<DELIM > "$DIR"/reboot_fc1.bash
 #!/bin/bash
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-sim.pem ubuntu@$SIM_IP "tail -1 /var/log/dpkg.log"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-fc1.pem ubuntu@$FC1_IP "sudo reboot"
 DELIM
-chmod +x $DIR/dpkg_log_sim.bash
+chmod +x "$DIR"/reboot_fc1.bash
 
 
 # --------------------------------------------
 
 
-cat <<DELIM > $DIR/find_file_sim.bash
+cat <<DELIM > "$DIR"/dpkg_log_fc2.bash
 #!/bin/bash
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-sim.pem ubuntu@$SIM_IP "ls \$1"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-fc2.pem ubuntu@$FC2_IP "tail -1 /var/log/dpkg.log"
 DELIM
-chmod +x $DIR/find_file_sim.bash
+chmod +x "$DIR"/dpkg_log_fc2.bash
+
 
 # --------------------------------------------
 
 
-cat <<DELIM > $DIR/ssh_sim.bash
+cat <<DELIM > "$DIR"/find_file_fc2.bash
 #!/bin/bash
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-sim.pem ubuntu@$SIM_IP
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-fc2.pem ubuntu@$FC2_IP "ls \$1"
 DELIM
-chmod +x $DIR/ssh_sim.bash
+chmod +x "$DIR"/find_file_fc2.bash
 
 # --------------------------------------------
 
 
-cat <<DELIM > $DIR/sim_init.bash
+cat <<DELIM > "$DIR"/ssh_fc2.bash
+#!/bin/bash
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-fc2.pem ubuntu@$FC2_IP
+DELIM
+chmod +x "$DIR"/ssh_fc2.bash
+
+
+# --------------------------------------------
+
+
+cat <<DELIM > "$DIR"/reboot_fc2.bash
+#!/bin/bash
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-fc2.pem ubuntu@$FC2_IP "sudo reboot"
+DELIM
+chmod +x "$DIR"/reboot_fc2.bash
+
+
+# --------------------------------------------
+
+
+cat <<DELIM > "$DIR"/fc2_init.bash
 #!/bin/bash
 set -ex
-exec > $DIR/sim_init.log 2>&1
+exec > "$DIR"/fc2_init.log 2>&1
 
-chmod +x $DIR/sim_startup_script.bash
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-sim.pem $DIR/sim_startup_script.bash ubuntu@$SIM_IP:sim_startup_script.bash
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-sim.pem ubuntu@$SIM_IP "nohup sudo ./sim_startup_script.bash > ssh_startup.out 2> ssh_startup.err < /dev/null &"
+chmod +x "$DIR"/fc2_startup_script.bash
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-fc2.pem "$DIR"/fc2_startup_script.bash ubuntu@$FC2_IP:fc2_startup_script.bash
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-fc2.pem ubuntu@$FC2_IP "nohup sudo ./fc2_startup_script.bash > ssh_startup.out 2> ssh_startup.err < /dev/null &"
 
 DELIM
-chmod +x $DIR/sim_init.bash
+chmod +x "$DIR"/fc2_init.bash
 
 
 # --------------------------------------------
 
 
 
-cat <<DELIM > $DIR/reboot_sim.bash
+cat <<DELIM > "$DIR"/dpkg_log_sim.bash
 #!/bin/bash
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-sim.pem ubuntu@$SIM_IP "sudo reboot"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-sim.pem ubuntu@$SIM_IP "tail -1 /var/log/dpkg.log"
 DELIM
-chmod +x $DIR/reboot_sim.bash
+chmod +x "$DIR"/dpkg_log_sim.bash
+
 
 # --------------------------------------------
 
-cat <<DELIM > $DIR/ping_gazebo.bash
+
+cat <<DELIM > "$DIR"/find_file_sim.bash
 #!/bin/bash
-ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-sim.pem ubuntu@$SIM_IP ". /usr/share/drcsim/setup.sh; timeout -k 1 5 gztopic list"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-sim.pem ubuntu@$SIM_IP "ls \$1"
 DELIM
-chmod +x $DIR/ping_gazebo.bash
+chmod +x "$DIR"/find_file_sim.bash
 
 # --------------------------------------------
-cat <<DELIM > $DIR/ping_gl.bash
+
+
+cat <<DELIM > "$DIR"/ssh_sim.bash
 #!/bin/bash
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-sim.pem ubuntu@$SIM_IP "DISPLAY=localhost:0 timeout -k 1 5 glxinfo"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-sim.pem ubuntu@$SIM_IP
 DELIM
-chmod +x $DIR/ping_gl.bash
+chmod +x "$DIR"/ssh_sim.bash
 
 # --------------------------------------------
 
-cat <<DELIM > $DIR/stop_sim.bash
+
+cat <<DELIM > "$DIR"/sim_init.bash
+#!/bin/bash
+set -ex
+exec > "$DIR"/sim_init.log 2>&1
+
+chmod +x "$DIR"/sim_startup_script.bash
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-sim.pem "$DIR"/sim_startup_script.bash ubuntu@$SIM_IP:sim_startup_script.bash
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-sim.pem ubuntu@$SIM_IP "nohup sudo ./sim_startup_script.bash > ssh_startup.out 2> ssh_startup.err < /dev/null &"
+
+DELIM
+chmod +x "$DIR"/sim_init.bash
+
+
+# --------------------------------------------
+
+
+
+cat <<DELIM > "$DIR"/reboot_sim.bash
+#!/bin/bash
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-sim.pem ubuntu@$SIM_IP "sudo reboot"
+DELIM
+chmod +x "$DIR"/reboot_sim.bash
+
+# --------------------------------------------
+
+cat <<DELIM > "$DIR"/ping_gazebo.bash
+#!/bin/bash
+ssh -t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-sim.pem ubuntu@$SIM_IP ". /usr/share/drcsim/setup.sh; timeout -k 1 5 gztopic list"
+DELIM
+chmod +x "$DIR"/ping_gazebo.bash
+
+# --------------------------------------------
+cat <<DELIM > "$DIR"/ping_gl.bash
+#!/bin/bash
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-sim.pem ubuntu@$SIM_IP "DISPLAY=localhost:0 timeout -k 1 5 glxinfo"
+DELIM
+chmod +x "$DIR"/ping_gl.bash
+
+# --------------------------------------------
+
+cat <<DELIM > "$DIR"/stop_sim.bash
 #!/bin/bash
 sudo stop vrc_netwatcher
 sudo stop vrc_bytecounter
 sudo redis-cli set vrc_current_outbound_latency 0
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-sim.pem ubuntu@$SIM_IP "bash cloudsim/stop_sim.bash"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-sim.pem ubuntu@$SIM_IP "bash cloudsim/stop_sim.bash"
 sudo iptables -F FORWARD
 DELIM
-chmod +x $DIR/stop_sim.bash
+chmod +x "$DIR"/stop_sim.bash
 
 # --------------------------------------------
 
-cat <<DELIM > $DIR/start_sim.bash
+cat <<DELIM > "$DIR"/start_sim.bash
 #!/bin/bash
 
 MAX_TIME=30
@@ -287,7 +287,7 @@ sudo iptables -F FORWARD
 sudo stop vrc_netwatcher
 sudo stop vrc_bytecounter
 sudo start vrc_netwatcher
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-sim.pem ubuntu@$SIM_IP "nohup bash cloudsim/start_sim.bash \$1 \$2 \$3 > ssh_start_sim.out 2> ssh_start_sim.err < /dev/null"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-sim.pem ubuntu@$SIM_IP "nohup bash cloudsim/start_sim.bash \$1 \$2 \$3 > ssh_start_sim.out 2> ssh_start_sim.err < /dev/null"
 
 tstart=\$(date +%s)
 bash /home/ubuntu/cloudsim/ping_gazebo.bash
@@ -303,12 +303,12 @@ while [[ \$? -ne 0 ]]; do
 done
 
 DELIM
-chmod +x $DIR/start_sim.bash
+chmod +x "$DIR"/start_sim.bash
 
 
 # --------------------------------------------
 
-cat <<DELIM > $DIR/copy_net_usage.bash
+cat <<DELIM > "$DIR"/copy_net_usage.bash
 #!/bin/bash
 
 # 1. Copy the directory containing the JSON task file and the network usage to the simulator
@@ -333,11 +333,11 @@ scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/ubuntu/
 
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/ubuntu/cloudsim/key-sim.pem ubuntu@10.0.0.51 bash /home/ubuntu/cloudsim/send_to_portal.bash \$1 \$2 /home/ubuntu/ubuntu-portal.key vrcportal-test.osrfoundation.org >> copy_net_usage.log 2>&1
 DELIM
-chmod +x $DIR/copy_net_usage.bash
+chmod +x "$DIR"/copy_net_usage.bash
 
 # --------------------------------------------
 
-cat <<DELIM > $DIR/get_sim_logs.bash
+cat <<DELIM > "$DIR"/get_sim_logs.bash
 #!/bin/bash
 
 # Get state.log and score.log from the simulator 
@@ -363,25 +363,25 @@ then
 fi
 
 DELIM
-chmod +x $DIR/get_sim_logs.bash
+chmod +x "$DIR"/get_sim_logs.bash
 
 echo "done :-)"
 
 
-cat <<DELIM > $DIR/update_constellation.bash
+cat <<DELIM > "$DIR"/update_constellation.bash
 #!/bin/bash
 set -ex
-exec > $DIR/update_constellation.log 2>&1
+exec > "$DIR"/update_constellation.log 2>&1
 
 echo "TODO: update_drcsim.bash"
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-sim.pem ubuntu@$SIM_IP "nohup sudo cloudsim/update_drcsim.bash > update_drcsim_sim.out 2> update_drcsim_sim.err < /dev/null"
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-fc1.pem ubuntu@$FC1_IP "nohup sudo cloudsim/update_drcsim.bash > update_drcsim_fc1.out 2> update_drcsim_sim.err < /dev/null"
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $DIR/key-fc2.pem ubuntu@$FC2_IP "nohup sudo cloudsim/update_drcsim.bash > update_drcsim_fc2.out 2> update_drcsim_sim.err < /dev/null"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-sim.pem ubuntu@$SIM_IP "nohup sudo cloudsim/update_drcsim.bash > update_drcsim_sim.out 2> update_drcsim_sim.err < /dev/null"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-fc1.pem ubuntu@$FC1_IP "nohup sudo cloudsim/update_drcsim.bash > update_drcsim_fc1.out 2> update_drcsim_sim.err < /dev/null"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i "$DIR"/key-fc2.pem ubuntu@$FC2_IP "nohup sudo cloudsim/update_drcsim.bash > update_drcsim_fc2.out 2> update_drcsim_sim.err < /dev/null"
 
 # update local packages on the router
 sudo cloudsim/update_drcsim.bash &
 
 DELIM
-chmod +x $DIR/update_constellation.bash
+chmod +x "$DIR"/update_constellation.bash
 
