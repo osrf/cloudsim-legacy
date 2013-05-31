68bfc5794338d52e61fdfc29310afce878744356
gerkey <gerkey@osrfoundation.org>
2013-05-31 01:52:42 +0000

    removed unneeded variable

--- js/tasks_list_widget.js
+++ js/tasks_list_widget.js
@@ -495,7 +495,6 @@ function add_task_widget(const_div, constellation_name, task_id, state, task_tit
 
     x_button.onclick =  function()
     {
-        var title = task_title_div.innerHTML;
         var r=confirm('Delete task?'  );
         if (r==false)
         {
