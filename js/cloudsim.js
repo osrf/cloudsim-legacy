7bc04b5261dcb1306aca77b0e427ab51d5aa24b0
Hugo Boyer <hugo@osrfoundation.org>
2014-01-27 18:08:00 -0800

    create_start_stop_service done

--- js/cloudsim.js
+++ js/cloudsim.js
@@ -62,7 +62,7 @@ function create_constellation(div_name, configuration, constellation, machine_da
 
     		if (machine_name == "router")
     		{
-    			create_gzweb_widget(machine_div, constellation, machine_name, "sim_glx_state", "gazebo", "gzweb", "router_public_ip");
+    			create_gzweb_widget(machine_div, constellation, machine_name, "sim_glx_state", "gazebo", "gzweb", machine_data["router_public_ip"]);
     		}
     		
     		create_latency_widget(machine_div, constellation, machine_name, latency_key, latency_msg, max_latency);
@@ -98,7 +98,7 @@ function create_constellation(div_name, configuration, constellation, machine_da
         create_machine_launch_monitor_widget(machine_div, constellation, machine_name, "sim_launch_msg", "sim_state");
         create_machine_state_widget(machine_div, constellation, machine_name,"sim_aws_state");
         create_simulator_state_widget(machine_div, constellation, machine_name, "sim_glx_state", "gazebo");
-        create_gzweb_widget(machine_div, constellation, machine_name, "sim_glx_state", "gazebo", "gzweb", "sim_public_ip");
+        create_gzweb_widget(machine_div, constellation, machine_name, "sim_glx_state", "gazebo", "gzweb", machine_data["sim_public_ip"]);
         create_latency_widget(machine_div, constellation, machine_name, "sim_latency", "RTT latency between the simulator and OCU over the VPN", 1.1);
     }
 
--- js/gzweb_widget.js
+++ js/gzweb_widget.js
@@ -1,24 +1,25 @@
 
-function create_sim_service(machine_div,
+function create_start_stop_service(machine_div,
 							constellation_name,
 							machine_name,
 							title,
-							run_msg,
-							service_state_key,
+							link_txt,
+							service_state_key,  // "running", "starting", "stopping"
 							start_func,
 							stop_func,
-							start_timeout)
+							btn_timeout,
+							is_sim_service)
 {
 	var glx_key = "sim_glx_state";
 	var simulator_key = "gazebo";
 	var ip_key = "sim_public_ip";
-
 	console.log("create_sim_service '" + title + "' for " + constellation_name);
 	
-    var widget_div = _create_empty_widget(machine_div, "gzweb");
+    var widget_div = _create_empty_widget(machine_div, service_state_key);
     var status = status_img("gray");
     widget_div.innerHTML = status + "<b>" + title +"</b>";
 
+    // start button
     var start_button = document.createElement('input');
     start_button.setAttribute('type','button');
     start_button.setAttribute('value','Start');
@@ -28,10 +29,11 @@ function create_sim_service(machine_div,
     	start_button.disabled = true;
         setTimeout( function(){
         	start_button.disabled = false;
-            }, 10000); // setTimeOut
+            }, btn_timeout); // setTimeOut
     }
     widget_div.appendChild(start_button);
 
+    // stop button
     var stop_button = document.createElement('input');
     stop_button.setAttribute('type','button');
     stop_button.setAttribute('value','Stop');
@@ -41,10 +43,78 @@ function create_sim_service(machine_div,
     	stop_button.disabled = true;
         setTimeout( function(){
         	stop_button.disabled = false;
-         }, 5000); // setTimeOut
+         }, btn_timeout); // setTimeOut
     }
     widget_div.appendChild(stop_button);
+    
+    var link = document.createElement('span');
+    widget_div.appendChild(link);
+    
+    var count = 0;
+    var img = widget_div.querySelector("img");
+    $.subscribe("/constellation", function(event, data){
+        if(data.constellation_name != constellation_name)
+            return;       
+        if (count == 100) 
+            count =0;
+        else 
+            count ++;     
+        var simulator_running = false;        
+        var service_state = data[service_state_key];
 
+        // X and simulator must be running
+        if (data[glx_key] == "running")
+        {
+            if(data[simulator_key] == "running")
+            {
+            	simulator_running = true;
+            }
+        }
+        
+        if(is_sim_service)
+        {
+        	if (simulator_running == false)
+	        {
+	        	img.src = "/js/images/red_status.png";
+	        	if(link.innerHTML != "")
+	        		link.innerHTML = "";
+	        	stop_button.disabled = true;
+	            start_button.disabled = true;        	
+	        	return;
+	        }      
+        }
+        if (service_state == "running")
+        {
+            img.src = "/js/images/blue_status.png";
+            if (link.innerHTML != link_txt)
+            {
+            	link.innerHTML = link_txt;
+            }
+            stop_button.disabled = false;
+            start_button.disabled = true;
+        }
+        else if (service_state == "starting")
+        {
+        	colors =  ["/js/images/gray_status.png", "/js/images/blue_status.png"];
+        	var color = colors[count % colors.length];
+        	img.src = color;
+        }	
+        else if (service_state == "stopping")
+        {
+        	colors =  ["/js/images/gray_status.png", "/js/images/red_status.png"];
+        	var color = colors[count % colors.length];
+        	img.src = color;
+        }
+        else
+        {
+        	if(link.innerHTML != "")
+        		link.innerHTML = "";
+            img.src = "/js/images/gray_status.png";
+            stop_button.disabled = true;
+            start_button.disabled = false;
+            return;
+        }
+    });
 }
 
 function create_gzweb_widget(machine_div,
@@ -53,135 +123,33 @@ function create_gzweb_widget(machine_div,
 		 glx_key,
 		 simulator_key,
 		 gz_web_key,
-		 ip_key)
+		 ip_address)
 {
-	create_sim_service(machine_div, constellation_name, machine_name,
-			"WebGL interface",
-			);
-}
+	var title = "WebGL interface";
+	var service_state_key = "gzweb"; // gz_web_key
+	var btn_timeout = 10000;
 
-function create_gzweb_widget2(machine_div,
-						     constellation_name,
-						     machine_name,
-							 glx_key,
-							 simulator_key,
-							 gz_web_key,
-							 ip_key)
-{
-	console.log("create_gzweb_widget for " + constellation_name);
+    var web_url = ip_address + ":8080";
+    var notebook_url = ip_address + ":8888";
+    var link_txt = '<a href=http://' + web_url + ' target="_blank" >3D view</a> ' ;
+    link_txt += '<a href=http://' + notebook_url + ' target="_blank" >Python notebook</a>' ;
+
+	var start_func = function(){
+		start_web_tools(constellation_name);
+	}
 	
-    var widget_div = _create_empty_widget(machine_div, "gzweb");
-    var status = status_img("gray");
-    widget_div.innerHTML = status + "<b>WebGL interface</b>";
+	var stop_func = function(){
+		stop_web_tools(constellation_name);
+	}
 
-    var start_button = document.createElement('input');
-    start_button.setAttribute('type','button');
-    start_button.setAttribute('value','Start');
-    start_button.onclick =  function()
-    {
-    	start_web_tools(constellation_name);
-    	start_button.disabled = true;
-        setTimeout( function(){
-        	start_button.disabled = false;
-            }, 10000); // setTimeOut
-    }
-    widget_div.appendChild(start_button);
-
-    var stop_button = document.createElement('input');
-    stop_button.setAttribute('type','button');
-    stop_button.setAttribute('value','Stop');
-    stop_button.onclick =  function()
-    {
-    	stop_web_tools(constellation_name);
-    	stop_button.disabled = true;
-        setTimeout( function(){
-        	stop_button.disabled = false;
-         }, 5000); // setTimeOut
-    }
-    widget_div.appendChild(stop_button);
- 
-    var link = document.createElement('span');
-    //link.innerHTML = "hello";
-    widget_div.appendChild(link);
-    
-    var count = 0;
-    var link_txt = "";
-    var img = widget_div.querySelector("img");
-    $.subscribe("/constellation", function(event, data){
-        if(data.constellation_name != constellation_name)
-            return;
-        
-        if (count == 100) 
-            count =0;
-        else 
-            count ++;
-        
-        var gzweb_running = false;
-        var simulator_running = false;
-        
-        
-        // X and simulator must be running
-        if (data[glx_key] == "running")
-        {
-            if(data[simulator_key] == "running")
-            {
-            	simulator_running = true;
-            }
-            else
-            {
-            	img.src = "/js/images/red_status.png";
-            	link_txt = "";
-            	if(link.innerHTML != link_txt)
-            		link.innerHTML = link_txt;
-            	stop_button.disabled = true;
-                start_button.disabled = true;
-                return;
-            }
-        }
-        if (simulator_running == false)
-        {
-        	img.src = "/js/images/red_status.png";
-        	link_txt = "";
-        	if(link.innerHTML != link_txt)
-        		link.innerHTML = link_txt;
-        	stop_button.disabled = true;
-            start_button.disabled = true;        	
-        	return;
-        }
-        
-        if (data[gz_web_key] == "running")
-        {
-            img.src = "/js/images/blue_status.png";
-            var web_url = data[ip_key] + ":8080";
-            var notebook_url = data[ip_key] + ":8888";
-            link_txt = '<a href=http://' + web_url + ' target="_blank" >3D view</a> ' ;
-            link_txt += '<a href=http://' + notebook_url + ' target="_blank" >Python notebook</a>' ;
-            if (link.innerHTML != link_txt)
-            {
-            	link.innerHTML = link_txt;
-            }
-            stop_button.disabled = false;
-            start_button.disabled = true;
-        }
-        else if (data[gz_web_key] == "starting")
-        {
-        	colors =  ["/js/images/gray_status.png", "/js/images/blue_status.png"];
-        	var color = colors[count % colors.length];
-        	img.src = color;
-        }	
-        else if (data[gz_web_key] == "stopping")
-        {
-        	colors =  ["/js/images/gray_status.png", "/js/images/red_status.png"];
-        	var color = colors[count % colors.length];
-        	img.src = color;
-        }
-        else
-        {
-        	link.innerHTML = "";
-            img.src = "/js/images/gray_status.png";
-            stop_button.disabled = true;
-            start_button.disabled = false;
-            return;
-        }
-    });
+	create_start_stop_service( machine_div,
+						constellation_name,
+						machine_name,
+						title,
+						link_txt,
+						service_state_key,
+						start_func,
+						stop_func,
+						btn_timeout,
+						true);
 }
